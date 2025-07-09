927bb780ee483b4b1d173ad3335646bf4055dcb2
Kobe Amoh <kobexamoh@gmail.com>
2025-07-09 10:11:59 -0600

    format

--- src/views/SearchBar.jsx
+++ src/views/SearchBar.jsx
@@ -1,19 +1,21 @@
 export default function SearchBar() {
-    function search(formData) {
-        const query = formData.get("search");
-        alert(`You're looking for '${query}'`);
-    }
-    return (
-      <div class="mt-4">
-        <form action={search}>
-          <input
-            name="search"
-            className="border-2 border-gray-300 px-4 focus:border-sky-500 rounded-lg py-2 w-10/12"
-            placeholder="Enter a name, party, city, or postal code..."
-          />
-          <button type="submit" className="w-2/12">Search</button> 
-          {/* TODO: replace with search icon */}
-        </form>
-      </div>
-    );
+  function search(formData) {
+    const query = formData.get("search");
+    alert(`You're looking for '${query}'`);
+  }
+  return (
+    <div class="mt-4">
+      <form action={search}>
+        <input
+          name="search"
+          className="border-2 border-gray-300 px-4 focus:border-sky-500 rounded-lg py-2 w-10/12"
+          placeholder="Enter a name, party, city, or postal code..."
+        />
+        <button type="submit" className="w-2/12">
+          Search
+        </button>
+        {/* TODO: replace with search icon */}
+      </form>
+    </div>
+  );
 }
