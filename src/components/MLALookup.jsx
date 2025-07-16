8071678f93c2c18fba995cb358de9ddcc247ef95
Kobe Amoh <kobexamoh@gmail.com>
2025-07-16 13:37:10 -0600

    Begin scaffolding lookup

---
+++ src/components/MLALookup.jsx
@@ -0,0 +1,12 @@
+
+
+export default function MLALookup() {
+    return(
+        <div>
+            <h2>MLA Finder</h2>
+            <ul>
+                <li></li>
+            </ul>
+        </div>
+    )
+}
