7c7778430faaa39bf3d9cf906519754e2b314166
Kobe Amoh <kobexamoh@gmail.com>
2025-07-20 17:05:12 -0600

    Mock initial success/failure logic

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,1 +1,16 @@
 import Card from "./Card";
+import Success from "./Success";
+import Error from "./Error";
+import { useState } from "react";
+
+
+
+export default function Result() {
+    const [loading, setLoading] = useState(false);
+    
+    if (loading) {
+            return <Success />   
+    } else {
+        return <Error />
+    }
+}
