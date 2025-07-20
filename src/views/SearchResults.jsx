ceb0e25610110236e6d058b7160a8e6cb825717e
Kobe Amoh <kobexamoh@gmail.com>
2025-07-20 17:55:28 -0600

    add state to search results

--- src/views/SearchResults.jsx
+++ src/views/SearchResults.jsx
@@ -1,11 +1,13 @@
+import { useState } from "react";
 import Success from "../components/Success";
 import Error from "../components/Error";
 
 export default function SearchResults () {
+    const [results, setResults] = useState(false);
     if (results) {
-        <Success />
+        return <Success />
     } else {
         // TODO: change to render the NoResults view
-        <Error />
+        return <Error />
     }
 }
