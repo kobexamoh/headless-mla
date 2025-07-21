ec2dc8f0ddf210c356778bd6029c2d741fa781fe
Kobe Amoh <kobexamoh@gmail.com>
2025-07-20 18:13:08 -0600

    Begin fetch api use

--- src/views/SearchResults.jsx
+++ src/views/SearchResults.jsx
@@ -1,9 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Success from "../components/Success";
 import Error from "../components/Error";
 
 export default function SearchResults () {
     const [results, setResults] = useState(false);
+    
+    useEffect(() => {
+        const fetchMembers = async () => {
+            const baseURL = "https://findmymla.social/wp-json/wp/v2/mla";
+            try {
+                const response = await fetch(baseURL);
+                if (!response.ok) {
+                    throw new Error(`Response status: ${response.status}`);
+                }
+            }
+        } catch (err) {
+
+        } finally {
+
+        }l
+    }, []);
+    
     if (results) {
         return <Success />
     } else {
